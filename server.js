8ceb95180c59db773c095baae37b878919e90619
jenil-simform <jenil@simformsolutions.com>
2023-03-17 14:01:20 +0530

    errorhandling in file upload added

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+// const cors = require('cors')
+const helmet = require('helmet')
 
 require('dotenv').config();
 require('./src/db/mongoose')
-
 
 //importing routes
 const userRoutes = require('./src/routers/userRoute')
 
 const app = express();
+
+/*
+* we can configure cors policy using below line, but currently
+* i am using same origin policy so no other domain can make request to
+* my server, but by cors configuration we can set which domains have allowed
+* to make which type of requests [get, post etc.] of my server.
+*/
+
+// app.use(cors())
+
+
+//configuring helmet for security reasons
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"]
+        },
+    })
+)
+
+//configuring rendering engine
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 
 app.use(userRoutes);
+
 
 //server configuration
 const port = 8000;
@@ -26,4 +51,3 @@ const host = 'localhost'
 app.listen(port, () => {
     console.log(`server is listening on http://${host}:${port}`);
 })
-    
--- src/errorHandlers/imageValidationError.js
+++ src/errorHandlers/imageValidationError.js
@@ -1,4 +1,10 @@
-const errorHandler = (error, req, res, next) => {
+
+/**
+ * Middleware function to handle image validation errors when uploading via Multer.
+ * Checks whether the image is in the correct format (JPEG, PNG, JPG, WEBP) and throws an error if not.
+ * Also Checks file size limit and throws an error if file size is larger
+ */
+const imageValidationErrorHandler = (error, req, res, next) => {
     return res.status(415).render('profile', {
         buffer : req.user.avatar,
         username : req.user.name,
@@ -7,4 +13,4 @@ const errorHandler = (error, req, res, next) => {
     // next()
 }
 
-module.exports = errorHandler;
+module.exports = imageValidationErrorHandler;
