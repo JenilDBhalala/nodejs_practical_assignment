e6feb382e0caaaa358fac8d879e4112f34474dcd
jenil-simform <jenil@simformsolutions.com>
2023-03-23 18:37:07 +0530

    some changes

--- server.js
+++ server.js
@@ -40,13 +40,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
 app.use(userRoutes);
 
-//server configuration
-const port = 8000;
-const host = 'localhost'
 
-app.listen(port, () => {
-    console.log(`server is listening on http://${host}:${port}`);
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST;
+
+//server configuration
+app.listen(PORT, () => {
+    console.log(`server is listening on http://${HOST}:${PORT}`);
 })
